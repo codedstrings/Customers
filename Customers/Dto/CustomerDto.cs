e07cb0c8a305b897a37901cf6666d72e49744f74
Mridhul M L <mridhulml@gmail.com>
2024-03-14 19:33:26 +0530

    add: CustomerDto, GetCustomerDetails(); change: insert logic to AddToDb()

---
+++ Customers/Dto/CustomerDto.cs
@@ -0,0 +1,16 @@
+﻿using Customers.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Customers.Dto
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+        public string  Name { get; set; }
+        //public CustomerAddress CustomerAdress { get; set; }
+    }
+}
--- Customers/Program.cs
+++ Customers/Program.cs
@@ -1,5 +1,7 @@
 ﻿using Customers.db;
+using Customers.Dto;
 using Customers.models;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Win32.SafeHandles;
 
 namespace Customers
@@ -7,12 +9,29 @@ namespace Customers
    
     public class Program
     {
+        static List<CustomerDto> GetCustomerDetails()
+        {
+            CustomersDbContext _dbContext = new CustomersDbContext();
+            List<Customer> customerList = _dbContext.Customers.ToList();
+            List<CustomerDto> selectedCustomer = new List<CustomerDto>();
+            //List<Customer>
+            foreach (Customer item in customerList)
+            {
+                selectedCustomer.Add(new CustomerDto()
+                {   Id = item.Id,
+                    Name = item.Name
+                });
+            }
+            return selectedCustomer;
+
+        }
         static string GetFromUser(string display)
         {
            Console.Write(display);
            return Console.ReadLine();
         }
-        static void Main(string[] args)
+
+        static bool AddToDb()
         {
             CustomersDbContext _dbContext = new CustomersDbContext();
 
@@ -33,17 +52,55 @@ namespace Customers
             {
                 Console.WriteLine($"OrderNo.{i}");
                 Order order = new Order
-                {   
+                {
                     OrderNumber = GetFromUser("OrderCode: "),
                     OrderQuantity = int.Parse(GetFromUser("Quantity? ")),
                 };
 
                 // Add the order to the customer's Orders list
                 customer1.Orders.Add(order);
-               
+
             }
             _dbContext.Customers.Add(customer1);
             _dbContext.SaveChanges();
+
+            //return true for successful db insertion
+            if (true)
+            {
+                return true; 
+            }
+            else
+            {
+                return false;
+            }
+        }
+        static void Main(string[] args)
+        {
+            int choice = int.Parse(GetFromUser("1. Insert\n2. GetAll\n"));
+            switch (choice)
+            {
+                case 1: 
+                    var status = AddToDb();
+                    if (status)
+                    {
+                        Console.WriteLine("Successful!");
+                    }
+                    else Console.WriteLine("Unsuccessful!");
+                    break;
+
+                case 2:
+                    // Call the function to get customer details
+                    List<CustomerDto> customerDetails = GetCustomerDetails();
+
+                    // Print the details
+                    foreach (var customerDetail in customerDetails)
+                    {
+                        Console.WriteLine($"Customer ID: {customerDetail.Id}, Name: {customerDetail.Name}");
+                    }
+                    break;
+
+                default: break;
+            }
 
         }
     }
