152979f5579d17da7a33c7bd1d0b71785ea66288
Mridhul M L <mridhulml@gmail.com>
2024-03-16 12:37:58 +0530

    add: WeatherApiTest project

---
+++ WeatherApiTest/Helper/Weather.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApiTest.Helper
+{
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+    public class Daily
+    {
+        public List<string> time { get; set; }
+        public List<double> temperature_2m_max { get; set; }
+    }
+
+    public class DailyUnits
+    {
+        public string time { get; set; }
+        public string temperature_2m_max { get; set; }
+    }
+
+    public class Weather
+    {
+        public double latitude { get; set; }
+        public double longitude { get; set; }
+        public double generationtime_ms { get; set; }
+        public int utc_offset_seconds { get; set; }
+        public string timezone { get; set; }
+        public string timezone_abbreviation { get; set; }
+        public double elevation { get; set; }
+        public DailyUnits daily_units { get; set; }
+        public Daily daily { get; set; }
+    }
+}
---
+++ WeatherApiTest/Program.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using WeatherApiTest.Helper;
+
+namespace WeatherApiTest
+{
+    public class Program
+    {
+        //static async Task Main(string[] args)
+        static async Task Main (string[] args)
+        {
+            #region HttpGet
+            Console.WriteLine("Press any key to call API");
+            Console.ReadLine();
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = await client.GetAsync("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&daily=temperature_2m_max");
+            if (response.IsSuccessStatusCode)
+            {
+                string json = await response.Content.ReadAsStringAsync();
+                var weather = JsonConvert.DeserializeObject<Weather>(json);
+
+                Console.WriteLine($"Latitude: {weather.latitude}, Longtitude: {weather.longitude}");
+                Console.WriteLine($"Daily Max Temperatures:");
+
+                for (int i = 0; i < weather.daily.time.Count; i++)
+                {
+                    Console.WriteLine($"{weather.daily.time[i]}: {weather.daily.temperature_2m_max[i]}°C");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Failed to fetch data. Status code: " + response.StatusCode);
+            }
+            #endregion
+        }
+    }
+}
